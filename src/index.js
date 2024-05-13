54df29b758abfbb52d013f2ae075e9d2943fd035
fatimakhan21 <nazii.khan8215@gmail.com>
2024-05-12 20:36:10 -0800

    added basic javascript and updated some infomation.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "ef5c282521e544537bfb522d048f133f";
+  let apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={Johannesburg},&limit={5}&appid={ef5c282521e544537bfb522d048f133f}' ;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Johannesburg");
